9f94f89397fdb1ed729bb6b2f59e4587876446b7
mnezmah <mario.nezmah@gmail.com>
2021-01-06 20:31:41 +0100

    Add useState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const App = (props) => {
+  //state was always an object. Now it can be anything
+  const [count, setCount] = useState(props.count)
+  const [text, setText] = useState('')
+
+  return (
+    <div>
+      <p>Current {text || 'count'} is {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(0)}>reset</button>
+      <input type="text" vlaue={text} onChange={(e) => setText(e.target.value)} />
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count:0
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App count={2} />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
