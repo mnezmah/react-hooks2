af4607289baf700e46a971da68a18b888f5076f3
mnezmah <mario.nezmah@gmail.com>
2021-01-31 17:12:59 +0100

    Add useReducer hook

--- src/index.js
+++ src/index.js
@@ -1,32 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
 
-const App = (props) => {
-  //state was always an object. Now it can be anything
-  const [count, setCount] = useState(props.count)
-  const [text, setText] = useState('')
+const notesReducer = (state, action) => {
+  switch (action.type) {
+    case 'POPULATE_NOTES':
+      return action.notes
+    case 'ADD_NOTE':
+      return [
+        ...state,
+        {
+          title: action.title,
+          body: action.body
+        }
+      ]
+    case 'REMOVE_NOTE':
+      return state.filter((note) => note.title !== action.title)
+    default:
+      return state
+  }
+}
+
+const NoteApp = () => {
+  const [notes, dispatch] = useReducer(notesReducer, [])
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote = (e) => {
+    e.preventDefault()
+    dispatch({
+      type: 'ADD_NOTE',
+      title,
+      body
+    })
+    setTitle('')
+    setBody('')
+  }
+
+  const removeNote = (title) => {
+    dispatch({ type: 'REMOVE_NOTE', title })
+  }
+
+  useEffect(() => {
+    const notes = JSON.parse(localStorage.getItem('notes'))
+    if (notes) {
+      dispatch({ type: 'POPULATE_NOTES', notes })     // setNotes(notesData)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
 
   return (
     <div>
-      <p>Current {text || 'count'} is {count}</p>
-      <button onClick={() => setCount(count + 1)}>+1</button>
-      <button onClick={() => setCount(count - 1)}>-1</button>
-      <button onClick={() => setCount(0)}>reset</button>
-      <input type="text" vlaue={text} onChange={(e) => setText(e.target.value)} />
+      <h1>Notes</h1>
+      {notes.map(note => (
+        <Note key={note.title}
+          note={note}
+          noteRemoval={removeNote}
+        />
+      ))}
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input type="text"
+          placeholder='Title'
+          value={title}
+          onChange={(e) => { setTitle(e.target.value) }}
+        />
+        <textarea
+          cols="30"
+          placeholder='Content of your note'
+          rows="5" value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <button>add note</button>
+      </form>
     </div>
   )
 }
 
-App.defaultProps = {
-  count:0
+const Note = ({ note, noteRemoval }) => {
+  useEffect(() => {
+    console.log('setting the eff!')
+
+    return () => {
+      console.log('Cleaning up eff')
+    }
+  }, [])
+
+  return (
+    <div key={note.title}>
+      <h3>{note.title}</h3>
+      <p>{note.body}</p>
+      <button onClick={() => noteRemoval(note.title)}>x</button>
+    </div>
+  )
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App count={2} />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <NoteApp />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
